b9a9776146d9857f9461e45ef305a67c558cc5dd
zntKI <kaloyanivanov@hotmail.com>
2024-09-11 22:41:15 +0200

    Modified and expanded swerve logic
    Made so that in the beginning, the car slowly accelerates to its desired speed, after which it starts swerving and the player has the control to get it back on track

--- Customer/Assets/Scenes/AITestSceneStuff/DrunkAIMovement.cs
+++ Customer/Assets/Scenes/AITestSceneStuff/DrunkAIMovement.cs
@@ -6,62 +6,130 @@ public class DrunkAIMovement : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField]
-    float moveSpeed = 1f;
+    float targetMoveSpeed = 75f;
+    [SerializeField]
+    float moveSpeedIncreaseAmount = 5f;
 
     [Header("Swerving")]
-    [SerializeField]
-    float swerveTimerMinSec;
-    [SerializeField]
-    float swerveTimerMaxSec;
     [SerializeField]
     float swerveMultiplierMin;
     [SerializeField]
     float swerveMultiplierMax;
     [SerializeField]
     float swerveRotationDefaultAmount = 0.1f;
+    [SerializeField]
+    float playerTurnAmount = 2f;
 
-    float swerveTimer;
-    float currentSwerveCounter;
-    float currentSwerveMultipier;
-
+    MovementState state;
 
     Rigidbody rb;
 
+    float moveSpeed;
+
+    float swerveInputValue;
+    float currentSwerveMultipier;
+    float rotationWhenStartedSwerving;
+
     private void Awake()
     {
+        state = MovementState.Start;
         rb = GetComponent<Rigidbody>();
-    }
-
-    void Start()
-    {
-        SetSwerveTimer();
     }
 
     void Update()
     {
-        currentSwerveCounter += Time.deltaTime;
-        if (currentSwerveCounter >= swerveTimer)
-        {
-            currentSwerveMultipier = Random.Range(swerveMultiplierMin, swerveMultiplierMax);
-
-            //Debug.Log($"SwerveMultiplier amount: {currentSwerveCounter}");
-
-            SetSwerveTimer();
-        }
+        HandleState();
     }
+
 
     void FixedUpdate()
     {
-        transform.Rotate(0f, swerveRotationDefaultAmount * currentSwerveMultipier, 0f);
+        switch (state)
+        {
+            case MovementState.Swerving:
+
+                float swerveRotationAmount = swerveRotationDefaultAmount * currentSwerveMultipier; //swerve force
+                float playerRotationAmount = Mathf.Abs(swerveRotationAmount) * 2 * swerveInputValue; //player force
+                float rotationAmount = swerveRotationAmount + playerRotationAmount; //end force amount depending on input
+                
+                transform.Rotate(0f, rotationAmount, 0f);
+
+                bool hasCarReturnedToStartingRotation = currentSwerveMultipier < 0 ? transform.rotation.y > rotationWhenStartedSwerving
+                    : transform.rotation.y < rotationWhenStartedSwerving;
+                if (hasCarReturnedToStartingRotation)
+                {
+                    SetState(MovementState.PassengerControl);
+                }
+
+                break;
+            case MovementState.PassengerControl:
+
+                transform.Rotate(0f, swerveInputValue * playerTurnAmount, 0f);
+
+                break;
+            case MovementState.Accelerating:
+                break;
+            default:
+                break;
+        }
+
         rb.velocity = transform.forward * moveSpeed;
     }
 
-
-    void SetSwerveTimer()
+    void HandleState()
     {
-        swerveTimer = Random.Range(swerveTimerMinSec, swerveTimerMaxSec);
-        currentSwerveCounter = 0f;
-
-        //Debug.Log($"Swerve timer set: {swerveTimer}");
+        switch (state)
+        {
+            case MovementState.Start:
+                moveSpeed += moveSpeedIncreaseAmount * Time.deltaTime;
+                if (moveSpeed >= targetMoveSpeed)
+                {
+                    moveSpeed = targetMoveSpeed;
+                    SetState(MovementState.Swerving/*(MovementState)Random.Range(2, 4)*/);
+                }
+                break;
+            case MovementState.Swerving:
+                HandlePlayerInput();
+                break;
+            case MovementState.PassengerControl:
+                HandlePlayerInput(); //TODO: until you reach a waypoint!!!
+                break;
+            default:
+                break;
+        }
     }
+
+    void HandlePlayerInput()
+    {
+        swerveInputValue = Input.GetAxisRaw("Horizontal");
+    }
+
+    void SetState(MovementState stateToChangeTo)
+    {
+        state = stateToChangeTo;
+        switch (state)
+        {
+            case MovementState.Start:
+                break;
+            case MovementState.Swerving:
+                rotationWhenStartedSwerving = transform.rotation.y;
+                currentSwerveMultipier = Random.Range(swerveMultiplierMin, swerveMultiplierMax);
+                break;
+            case MovementState.PassengerControl:
+                break;
+            case MovementState.Accelerating:
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+
+public enum MovementState
+{
+    Start,
+    PassengerControl,
+    Swerving,
+    Accelerating
 }
