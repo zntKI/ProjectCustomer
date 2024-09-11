5b099cfc1d17521f1ca0c0f5f04676f23681d16f
zntKI <kaloyanivanov@hotmail.com>
2024-09-11 13:23:57 +0200

    Implement AI car movement

--- Customer/Assets/Scenes/AITestSceneStuff/DrunkAIMovement.cs
+++ Customer/Assets/Scenes/AITestSceneStuff/DrunkAIMovement.cs
@@ -4,18 +4,64 @@ using UnityEngine;
 
 public class DrunkAIMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField]
     float moveSpeed = 1f;
 
+    [Header("Swerving")]
+    [SerializeField]
+    float swerveTimerMinSec;
+    [SerializeField]
+    float swerveTimerMaxSec;
+    [SerializeField]
+    float swerveMultiplierMin;
+    [SerializeField]
+    float swerveMultiplierMax;
+    [SerializeField]
+    float swerveRotationDefaultAmount = 0.1f;
+
+    float swerveTimer;
+    float currentSwerveCounter;
+    float currentSwerveMultipier;
+
+
     Rigidbody rb;
 
-    void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    void Start()
+    {
+        SetSwerveTimer();
+    }
+
     void Update()
     {
-        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, moveSpeed * Time.deltaTime);
+        currentSwerveCounter += Time.deltaTime;
+        if (currentSwerveCounter >= swerveTimer)
+        {
+            currentSwerveMultipier = Random.Range(swerveMultiplierMin, swerveMultiplierMax);
+
+            //Debug.Log($"SwerveMultiplier amount: {currentSwerveCounter}");
+
+            SetSwerveTimer();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0f, swerveRotationDefaultAmount * currentSwerveMultipier, 0f);
+        rb.velocity = transform.forward * moveSpeed;
+    }
+
+
+    void SetSwerveTimer()
+    {
+        swerveTimer = Random.Range(swerveTimerMinSec, swerveTimerMaxSec);
+        currentSwerveCounter = 0f;
+
+        //Debug.Log($"Swerve timer set: {swerveTimer}");
     }
 }
