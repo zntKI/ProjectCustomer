67fcd490fe6c001df4d4948049fee9988015bb4e
Nikola <nikolagoev9@gmail.com>
2024-09-25 11:17:46 +0200

    Blinking tutorial buttons

--- Customer/Assets/Scripts/TutorialButtons.cs
+++ Customer/Assets/Scripts/TutorialButtons.cs
@@ -9,6 +9,11 @@ public class TutorialButtons : MonoBehaviour
     [SerializeField] Image DKey;
     [SerializeField] Image SKey;
 
+    [SerializeField] float _lerpSpeed = 4f;
+    Color primaryColor = Color.white;
+    Color secondaryColor = Color.gray;
+    private float t;
+
     [YarnCommand("enableDKey")]
     public void EnableDKey()
     {
@@ -31,5 +36,20 @@ public class TutorialButtons : MonoBehaviour
     public void DisableSKey()
     {
         SKey.enabled = false;
+    }
+
+    private void Update()
+    {
+        t += Time.deltaTime;
+
+        if (DKey.enabled) {
+
+            DKey.color = Color.Lerp(primaryColor, secondaryColor, Mathf.Abs(Mathf.Sin(t * _lerpSpeed)));
+        }
+
+        if (SKey.enabled)
+        {
+            SKey.color = Color.Lerp(primaryColor, secondaryColor, Mathf.Abs(Mathf.Sin(t * _lerpSpeed)));
+        }
     }
 }
