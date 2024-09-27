c707006dd6c2caf5995c75e1a1b1f8c2de17223a
zntKI <kaloyanivanov@hotmail.com>
2024-09-27 10:24:37 +0200

    Changes

--- Customer/Assets/Scripts/DrunkAIMovement.cs
+++ Customer/Assets/Scripts/DrunkAIMovement.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.Build;
 using UnityEngine;
 using Yarn.Unity;
 using static UnityEngine.GraphicsBuffer;
